390200ca73452406b6baf6ca8c520356d2d611c3
mlister3 <matthew.list56@outlook.com>
2023-02-13 06:48:12 -0500

    Copy from local repository

---
+++ setup.py
@@ -0,0 +1,34 @@
+# Import cx_Freeze
+import cx_Freeze
+import setuptools
+import os
+import sys
+
+os.environ['TCL_LIBRARY'] = r'C:\Users\matth\anaconda3\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Users\matth\anaconda3\tcl\tk8.6'
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = r'C:\Users\matth\anaconda3\Library\plugins\platforms'
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = r'C:\Users\matth\anaconda3\Library\plugins'
+
+# Set up the executables
+executables = [cx_Freeze.Executable("OP_Tool.py")]
+
+# Set up the build options
+build_options = {
+    "packages": ["pandas", "matplotlib", "sys", "os", "tkinter", "openpyxl", "jdcal", "et_xmlfile", "PyQt5.QtCore", "PyQt5.QtGui", "PyQt5.QtWidgets", "PyQt5.QtSvg", "PyQt5.QtPrintSupport", "atexit"],
+    "include_files": [
+        "QUERY_FILE_GOES_HERE",
+        r"C:\Users\matth\anaconda3\Library\plugins\platforms\qwindows.dll",
+        r"C:\Users\matth\anaconda3\Library\bin\mkl_intel_thread.1.dll",
+        r"C:\Users\matth\anaconda3\Library\bin\mkl_def.1.dll",
+        r"C:\Users\matth\anaconda3\pkgs\mkl-2021.4.0-haa95532_640\Library\bin\mkl_sequential.1.dll"
+    ]
+}
+
+sys.path.append(r'C:\Users\matth\anaconda3\Library\plugins')
+
+# Run the build process
+cx_Freeze.setup(
+    name = "OP_Tool",
+    options = {"build_exe": build_options},
+    executables = executables
+)
