24058c87889cbd670b59ade59af36870ab5b49ad
Matthew Lister <120275018+mlister3@users.noreply.github.com>
2023-02-23 07:25:08 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,13 @@ os.environ['TK_LIBRARY'] = r'C:\Users\matth\anaconda3\tcl\tk8.6'
 os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = r'C:\Users\matth\anaconda3\Library\plugins\platforms'
 os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = r'C:\Users\matth\anaconda3\Library\plugins'
 
+options = {
+    "build_exe": {
+        "copy_dependent_files": True,
+        "compressor": "upx"
+    }
+}
+
 # Set up the executables
 executables = [cx_Freeze.Executable("JN_OP_Tool.py")]
 
@@ -17,7 +24,7 @@ build_options = {
     "packages": ["warnings", "pandas", "matplotlib", "sys", "os", "tkinter", 
     "openpyxl", "jdcal", "et_xmlfile", "PyQt5.QtCore", "PyQt5.QtGui", "PyQt5.QtWidgets", 
     "PyQt5.QtSvg", "PyQt5.QtPrintSupport", "atexit", "xlsxwriter", "glob"],
-
+    
     "include_files": [
         "QUERY_FILE_GOES_HERE",
         r"C:\Users\matth\anaconda3\Library\plugins\platforms\qwindows.dll",
